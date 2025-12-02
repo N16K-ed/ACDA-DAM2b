e1674bc79e536c79a6ebdce79aa4804615ec1b3e
N16K-ed <fsannicceb@educacion.navarra.es>
2025-12-02 20:09:08 +0100

    Comienzo UT4_A5_GymManager(Thymeleaf)
    :)

---
+++ ACDA - Intelli/UT4_GymManager_SanNicolasFran/src/main/java/org/ut4/gymmanager/controller/web/GrupoMuscularWebController.java
@@ -0,0 +1,60 @@
+package org.ut4.gymmanager.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.ut4.gymmanager.model.GrupoMuscular;
+import org.ut4.gymmanager.service.GrupoMuscularService;
+
+import java.util.List;
+
+@Controller
+public class GrupoMuscularWebController {
+
+    private final GrupoMuscularService grupoMuscularService;
+
+    public GrupoMuscularWebController (GrupoMuscularService grupoMuscularService){
+        this.grupoMuscularService = grupoMuscularService;
+    }
+
+    @GetMapping("web/grupos")
+    public String listadoGrupos(Model model){
+        List<GrupoMuscular> listado =  grupoMuscularService.listarTodos();
+
+        model.addAttribute("grupos", listado);
+
+        return "grupos-list";
+    }
+
+    @GetMapping("web/grupos/nuevo")
+    public String nuevoGrupo(Model model){
+        GrupoMuscular nuevo = new GrupoMuscular();
+
+        model.addAttribute("grupoMuscular", nuevo);
+
+        return "grupo-form";
+    }
+
+    @PostMapping("web/grupos/guardar")
+    public String guardarGrupo(@ModelAttribute("grupoMuscular") GrupoMuscular grupoMuscular, Model model){
+
+        grupoMuscularService.guardar(grupoMuscular);
+
+        return "redirect:/web/grupos";
+    }
+
+    @GetMapping("web/grupos/{id}/editar")
+    public String editar(@PathVariable long id, Model model){
+
+        GrupoMuscular grupoMuscular = grupoMuscularService.buscarPorId(id);
+
+        //AQUI ME QUEDO
+
+
+        return "redirect:/web/grupos";
+    }
+
+}
